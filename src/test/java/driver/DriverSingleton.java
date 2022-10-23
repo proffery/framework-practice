6ec1f05a580ee16c685f40b551bf3ec919a7b810
Dmitry Shamko <proffery@gmail.com>
2022-10-23 18:36:05 +0300

    webdrivermanager

--- src/test/java/driver/DriverSingleton.java
+++ src/test/java/driver/DriverSingleton.java
@@ -1,21 +1,30 @@
 package driver;
 
+import com.github.mike10004.xvfbmanager.XvfbManager;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DriverSingleton {
     private static WebDriver driver;
 
     public static WebDriver getDriver() {
-        if (null == driver) {
-            if ("firefox".equals(System.getProperty("browser"))) {
-                WebDriverManager.firefoxdriver().setup();
-                driver = new FirefoxDriver();
+        if (null == driver){
+            switch (System.getProperty("browser")){
+                case "firefox": {
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                }
+                case "xvfb": {
+                    WebDriverManager.firefoxdriver().linux().setup();
+                }
+                default: {
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                }
             }
-            WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
             driver.manage().window().maximize();
         }
         return driver;
