85676d71466c0e913d1fd31d13d2ab37a37cec82
Dmitry Shamko <proffery@gmail.com>
2022-11-02 22:40:11 +0300

    log_and_screenshots

--- src/test/java/driver/DriverSingleton.java
+++ src/test/java/driver/DriverSingleton.java
@@ -1,7 +1,6 @@
 package driver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
@@ -37,7 +36,7 @@ public class DriverSingleton {
                 }
             }
 
-            driver.manage().window().setSize(new Dimension(1920, 1080));
+            driver.manage().window().maximize();
         }
         return driver;
     }
