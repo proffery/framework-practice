8d190b433bc021ce9df0d0b1fe9f217fc821603b
Dmitry Shamko <proffery@gmail.com>
2022-11-03 15:17:09 +0300

    refactor

--- src/test/java/driver/DriverSingleton.java
+++ src/test/java/driver/DriverSingleton.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
@@ -16,23 +17,23 @@ public class DriverSingleton {
             switch (System.getProperty("browser")) {
                 case "firefox": {
                     WebDriverManager.firefoxdriver().setup();
-//                    FirefoxOptions options = new FirefoxOptions();
-//                    options.setHeadless(true);
-                    driver = new FirefoxDriver(/*options*/);
+                    FirefoxOptions options = new FirefoxOptions();
+                    options.setHeadless(true);
+                    driver = new FirefoxDriver(options);
                     break;
                 }
                 case "edge": {
                     WebDriverManager.edgedriver().setup();
-//                    EdgeOptions options = new EdgeOptions();
-//                    options.addArguments("headless");
-                    driver = new EdgeDriver(/*options*/);
+                    EdgeOptions options = new EdgeOptions();
+                    options.addArguments("headless");
+                    driver = new EdgeDriver(options);
                     break;
                 }
                 default: {
                     WebDriverManager.chromedriver().setup();
-//                    ChromeOptions options = new ChromeOptions();
-//                    options.addArguments("headless");
-                    driver = new ChromeDriver(/*options*/);
+                    ChromeOptions options = new ChromeOptions();
+                    options.addArguments("headless");
+                    driver = new ChromeDriver(options);
                     break;
                 }
             }
